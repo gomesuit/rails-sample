c1076e205cd9dce0af9760ba2e86e5bff452f3f4
gomesuit <gomesuit@gmail.com>
2018-04-03 02:20:22 +0900

    mysql2

--- script/provision.sh
+++ script/provision.sh
@@ -1,3 +1,18 @@
 #/bin/bash -ex
 
+sudo yum install -y git
+
+git clone https://github.com/riywo/anyenv ~/.anyenv
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
+$SHELL -l
+
+sudo yum install -y gcc openssl-devel readline-devel zlib-devel
+
+anyenv install rbenv
+
+$SHELL -l
+
+rbenv install 2.4.3
+
 echo finish
