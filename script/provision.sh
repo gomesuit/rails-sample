d22a2f461a965f2fdf8eab52518443ea63f5d762
gomesuit <gomesuit@gmail.com>
2018-04-03 03:25:48 +0900

    save

--- script/provision.sh
+++ script/provision.sh
@@ -5,24 +5,26 @@ sudo yum install -y git
 git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
-$SHELL -l
+. ~/.bash_profile
 
 sudo yum install -y gcc openssl-devel readline-devel zlib-devel mariadb-devel
 
 anyenv install rbenv
 anyenv install ndenv
 
-$SHELL -l
+. ~/.bash_profile
 
 rbenv install 2.4.3
 ndenv install v8.11.0
-gem install bundler
 
 cd /vagrant
 
+gem install bundler
 bundle install --without development test
 
 sudo yum install -y mariadb-server
 sudo systemctl start mariadb
 
-echo ''
+echo 'export RAILS_ENV=production' >> ~/.bash_profile
+echo "export SECRET_KEY_BASE=$(bin/rails secret)" >> ~/.bash_profile
+echo 'export RAILS_SERVE_STATIC_FILES=1' >> ~/.bash_profile
